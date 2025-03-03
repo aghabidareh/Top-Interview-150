14bf2b51f79afe09f6334c17b5920a4aaae88f85
aghabidareh <aghabidareh@gmail.com>
2025-03-03 04:23:14 -0800

    Majority Element (better run time complexity)

--- 5-Majority-Element.py
+++ 5-Majority-Element.py
@@ -1,9 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        from collections import Counter
-        frequency = Counter(nums)
         size_of_array = len(nums)
-        for (key, val) in frequency.items():
-            if val > (size_of_array / 2):
-                return key
+        if size_of_array == 1:
+            return nums[0]
+
+        counter = 1
+        nums.sort()
+
+        for i in range(1, size_of_array):
+            if nums[i - 1] == nums[i]:
+                counter += 1
+            else:
+                if counter > size_of_array // 2:
+                    return nums[i - 1]
+
+        if counter > size_of_array // 2:
+            return nums[i - 1]
+
         return None
